d7e69ff894eb9d0b120063bb0bfc2f86332ab110
tandoh22 <tandohsamuel4u@gmail.com>
2026-06-08 16:53:51 +0000

    database change

--- backend/database.py
+++ backend/database.py
@@ -3,10 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from core.config import settings
 
-engine = create_engine(
-    settings.DATABASE_URL,
-    connect_args={"check_same_thread": False} if "sqlite" in settings.DATABASE_URL else {}
-)
+engine = create_engine(settings.DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
